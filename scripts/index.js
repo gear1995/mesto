07142c0346fa044940256ce202e21353eb08ebd6
Evgeniy Yurchenkov <gear1995V@gmail.com>
2022-05-23 22:30:32 +0500

    first rewiv

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,52 @@
-const editButton = document.querySelector(".edit-button");
+const editButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const closePopupButton = document.querySelector(".popup__close");
 
 const profileTitle = document.querySelector(".profile__title");
-const nameFieldElement = document.querySelector(".popup__input_name");
+const nameFieldElement = document.querySelector(
+  ".popup__input_place_field-name"
+);
 
 const profileSubtitle = document.querySelector(".profile__subtitle");
 const descriptionFieldElement = document.querySelector(
-  ".popup__input_description"
+  ".popup__input_place_field-description"
 );
 
 const formElement = document.querySelector(".popup__form");
 
+function popupIsOpen() {
+  popup.classList.add("popup_opened");
+}
+
+function popupIsClose() {
+  popup.classList.remove("popup_opened");
+}
+
 editButton.addEventListener("click", function () {
-  popup.classList.add("popup__opened");
+  popupIsOpen();
   nameFieldElement.value = profileTitle.textContent;
   descriptionFieldElement.value = profileSubtitle.textContent;
 });
 
 editButton.addEventListener("click", function () {
-  popup.classList.add("popup__opened");
+  popupIsOpen();
 });
 
 closePopupButton.addEventListener("click", function () {
-  popup.classList.remove("popup__opened");
+  popupIsClose();
 });
 
 formElement.addEventListener("submit", function (Event) {
   Event.preventDefault();
   profileTitle.textContent = nameFieldElement.value;
   profileSubtitle.textContent = descriptionFieldElement.value;
-  popup.classList.remove("popup__opened");
+  popupIsClose();
 });
 
 /* function detectClickOutside(event) {
   console.log(event.target);
   console.log(event.target.classList);
   if (event.target.classList.contains("popup")) {
-    popup.classList.remove("popup__opened");
+    popup.classList.remove("popup_opened");
   }
 } */
